b982604649faac145414edbfb69648f22e1935d2
Agustin Olivares <Faolivares3112@gmail.com>
2023-04-04 12:52:33 -0300

    Agrego comentarios

--- practicas/tp-trie/code/main.py
+++ practicas/tp-trie/code/main.py
@@ -3,5 +3,6 @@ import trie as tr
 Arbol = tr.Trie()
 # Caso 1: Inserto una palabra     # Funciona
 tr.insert(Arbol, "Hola")
-# Caso 2: Inserto una palabra distinta      #
+# Caso 2: Inserto varias palabras distintas      # >2 Falla
 tr.insert(Arbol, "Rapido")
+tr.insert(Arbol, "Javier")
