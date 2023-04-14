76397650e34ebd2e273c298adb697cbb123ab830
Agustin Olivares <Faolivares3112@gmail.com>
2023-04-13 21:20:10 -0300

    Modificacion para poder seleccionar longitud de la tabla

--- practicas/tp-hashtable/code/main.py
+++ practicas/tp-hashtable/code/main.py
@@ -1,7 +1,8 @@
 import dictionary as d
 
-list = [0, 0, 0]
 
+# Los comandos de abajo estan hechos para una lista de longitud 3
+list = [None]*int(input("Longitud de tabla = "))        # Asi puedo haer una lista de longitud n
 
 d.insert(list, 5, "Papá") #2
 d.insert(list, 9, "Mandarina") #0
@@ -11,7 +12,6 @@ d.insert(list, 24, "Pera") #0
 
 print(d.delete(list, 9))
 
+
 print(list)
-
-
 
