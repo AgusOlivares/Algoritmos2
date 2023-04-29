b5b5d1894c25b946caa6702175b4af8996c3b2ec
Agustin Olivares <Faolivares3112@gmail.com>
2023-04-28 21:38:36 -0300

    excercise 2, fix excercise 3

--- practicas/tp-grafos/code/graph.py
+++ practicas/tp-grafos/code/graph.py
@@ -4,7 +4,7 @@ class GraphNode:
     def __init__(self, key = None, color = "white") -> None:
         self.key = key 
         self.color = color
-        self.connect = []
+        self.connect = [] # funcionaria agregar un set() y en vez de append() un add()?
 
 class Graph:
 
@@ -31,8 +31,12 @@ class Graph:
         
     def link(self, vertice0, vertice1):
 
+        if vertice0 not in self._data or vertice1 not in self._data:
+            raise Exception("One or both vertices not in graph")
+        
         node0 = self._data[vertice0]
         node1 = self._data[vertice1]
+
         
         if vertice1 in node0.connect:
             raise Exception("Link already exist in graph")
@@ -40,8 +44,13 @@ class Graph:
         if vertice0 in node1.connect:
             raise Exception("Link already exist in graph")
         
+
+        if vertice0 == vertice1: node0.connect.append(vertice1); return
+
+
         node0.connect.append(vertice1)
         node1.connect.append(vertice0)
+        return
 
     def insert(self, vertice): 
 
@@ -62,3 +71,72 @@ class Graph:
             node = self._data[vertice]
             conexiones = sorted(node.connect)
             print(f"{vertice}: {conexiones}")
+
+    def existPath(self, graph, v0, v1):
+
+        if v0 not in graph._data or v1 not in graph._data:
+            raise Exception("One or both vs not in graph")
+        
+        if v0 == v1: return True
+        
+        for i in graph._data[v0].connect:
+            if v1 in graph._data[v0].connect:
+                return True
+            else:
+                return self.existPath(graph , i , v1)
+
+        return False
+    
+    def isConnected(self, graph):  ## RESOLVER Error
+
+        been = set([])
+
+        key = 0
+
+        lista = Graph.isConnected_recursive(graph, been, key)
+
+        return lista == graph.n
+        
+
+       
+
+    def isConnected_recursive(graph, been, key):
+
+        node = graph._data[key]
+        
+        for i in node.connect:
+            
+            if i in been:
+                continue
+            else:
+                been.add(i)
+
+            graph.isConnected_recursive(graph , been , i)
+        
+        return been
+
+    
+
+
+                    
+                
+
+            
+
+        
+
+
+
+        
+
+        
+
+        
+
+        
+        
+        
+
+
+
+
--- practicas/tp-grafos/code/main.py
+++ practicas/tp-grafos/code/main.py
@@ -1,8 +1,8 @@
 import graph as g
 
 # Initial Node and adjacency list
-vertex_list = [1, 2, 3, 4]
-adjacency_list = [(1, 2), (1, 4), (2, 3)]
+vertex_list = [1, 2, 3, 4, 5, 7, 8]
+adjacency_list = [(1, 2), (1, 4), (2, 3), (1, 1), (5, 1), (7, 8)]
 
 # Initialize graph
 graph = g.Graph(vertex_list)
@@ -10,14 +10,32 @@ graph.createGraph(vertex_list, adjacency_list)
 
 # Insert new Node
 graph.insert(0)
+#graph.insert(7)
+
 
 # Insert existing Node
 #graph.insert(1)
 
 # Create new Link
-graph.link(0, 1)
+graph.link(0, 3)
 
 # Try Existing link
 #graph.link(2, 1)
 
+# Create link of nonexisting node
+#graph.link(13, 2)
+
+print( "-"*20, " Graph ", "-"*20)
+print(graph.n)
 graph.printGraph()
+
+print( "-"*20, " ExistPath ", "-"*20)
+# Case 1: Direct Link
+print(graph.existPath(graph, 0, 1))
+
+print( "-"*20, " ExistPath ", "-"*20)
+print(graph.isConnected(graph))
+#print(graph._data.items())
+
+
+
