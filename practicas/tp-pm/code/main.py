5a979a98381e224a056dc2b6c22127d419ac8b6c
Agustin Olivares <Faolivares3112@gmail.com>
2023-05-20 18:49:26 -0300

    Ejercicio 7 y 8 (Obligatorios)

--- practicas/tp-pm/code/main.py
+++ practicas/tp-pm/code/main.py
@@ -2,7 +2,11 @@ import pm
 
 line = "radar"
 line2 = "anita lava la tina"
+line3 = "aaabccddd"
+line4 = "aaaallaaammmmmmadsgxfdxrrisxxco"
 
 print(pm.existChar(line, "a"))
 print(pm.isPalindrome(line2))
 print(pm.mostRepeatedChar(line2, "a"))
+print(pm.reduceLen(line3))
+print(pm.isContained(line4, "amarillo"))
--- practicas/tp-pm/code/pm.py
+++ practicas/tp-pm/code/pm.py
@@ -26,3 +26,33 @@ def mostRepeatedChar(String, c):
     return max(dict, key = dict.get)
 
 
+def reduceLen(String):
+
+    reduced_list = []
+    for char in String:
+
+            if char == (reduced_list and reduced_list[-1]):
+                reduced_list.pop()
+            else:
+                reduced_list.append(char)
+    return reduced_list
+
+def isContained(String, SubString):
+
+    long_SubString = len(SubString)
+
+    comprobation_array = [False] * long_SubString
+    i = 0
+
+    for char in String:
+        if char == SubString[i]:
+            comprobation_array[i] = True
+            i += 1
+        if i == long_SubString:
+            break
+    if comprobation_array.count(True) == long_SubString:
+        return True
+    else:
+        return False
+
+
